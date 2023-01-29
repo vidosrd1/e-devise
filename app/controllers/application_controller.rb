c3212d417c2b33e0edf4f1efbb0d3ecbf43bd4a3
Srđan Vidović <vidosrd@gmail.com>
2023-01-29 08:41:54 +0100

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   before_action :authenticate_user!
-=======
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +7,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
->>>>>>> 8f1d96dfe83aaae93162039c1239d786fb146f17
 end
